3ec6b71444179d24da1ee0f4b7213886c761f5ae
siddhant <siddhantdutta5260@gmail.com>
2025-04-19 12:32:13 +0530

    [Misc]: cleaned up CardOfferController.java

--- src/main/java/com/ccrecommender/controller/CardOfferController.java
+++ src/main/java/com/ccrecommender/controller/CardOfferController.java
@@ -3,6 +3,7 @@ package com.ccrecommender.controller;
 import com.ccrecommender.dto.OfferWithSavings;
 import com.ccrecommender.entity.CardOfferEntity;
 import com.ccrecommender.service.CardOfferService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,31 +11,31 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/offers")
+@RequiredArgsConstructor
 public class CardOfferController {
 
     private final CardOfferService cardOfferService;
 
-    public CardOfferController(CardOfferService cardOfferService) {
-        this.cardOfferService = cardOfferService;
-    }
-
+    // 🔹 Get all offers
     @GetMapping
-    public List<CardOfferEntity> getAllOffers() {
-        return cardOfferService.getAllCardOffers();
+    public ResponseEntity<List<CardOfferEntity>> getAllOffers() {
+        return ResponseEntity.ok(cardOfferService.getAllCardOffers());
     }
 
+    // 🔹 Create a new offer
     @PostMapping
-    public CardOfferEntity createOffer(@RequestBody CardOfferEntity offer) {
-
-        return cardOfferService.createCardOffer(offer);
+    public ResponseEntity<CardOfferEntity> createOffer(@RequestBody CardOfferEntity offer) {
+        CardOfferEntity created = cardOfferService.createCardOffer(offer);
+        return ResponseEntity.ok(created);
     }
 
-
+    // 🔹 Get offers by merchant
     @GetMapping("/merchant/{merchant}")
-    public List<CardOfferEntity> getOffersByMerchant(@PathVariable String merchant) {
-        return cardOfferService.getAllCardOffersByMerchant(merchant);
+    public ResponseEntity<List<CardOfferEntity>> getOffersByMerchant(@PathVariable String merchant) {
+        return ResponseEntity.ok(cardOfferService.getAllCardOffersByMerchant(merchant));
     }
 
+    // 🔹 Get best offers for a user (based on input + optional user cards)
     @GetMapping("/{userId}/best-offers")
     public ResponseEntity<List<OfferWithSavings>> getBestOffersForUser(
             @PathVariable Long userId,
